9753c23868773a2763ac5d70fc6f2ccc9a2120ed
Shalin Bhavsar <35373379+shalin24999@users.noreply.github.com>
2020-10-26 22:17:32 +0530

    Update countDown.js

--- countDown.js
+++ countDown.js
@@ -1,12 +1,13 @@
-let end = new Date("Sep 28, 2020 00:00:00");
+let end = new Date("Sep 28, 2020 00:00:00"); //End Time
 let endTime = end.getTime();
 document.getElementById("remTime").innerHTML = "Time Remaining until: " + end;
 
+//This function repeats itself after every one second
 let cntDwn = setInterval(function () {
-  let currTime = new Date().getTime();
+  let currTime = new Date().getTime(); //Get the current system time
   let diff = endTime - currTime;
 
-  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24)); 
   let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((diff % (1000 * 60)) / 1000);
@@ -15,7 +16,8 @@ let cntDwn = setInterval(function () {
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
-
+  
+  //After the timer expires
   if (diff <= 0) {
     clearInterval(cntDwn);
     document.getElementById("days").innerHTML = 0;
